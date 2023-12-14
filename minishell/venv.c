8698a422ab3591004a0508d7531a1ff3d4b6c0d0
aceyzz <data-xvi@protonmail.com>
2023-12-14 20:14:18 +0100

    Update minishell.h and venv.c files

--- minishell/venv.c
+++ minishell/venv.c
@@ -6,13 +6,42 @@
 /*   By: cedmulle <cedmulle@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 17:06:51 by cedmulle          #+#    #+#             */
-/*   Updated: 2023/12/14 19:23:42 by cedmulle         ###   ########.fr       */
+/*   Updated: 2023/12/14 20:14:07 by cedmulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 extern char	**environ;
+
+static void	increment_ascii(char *nbr_str)
+{
+	int		length;
+	int		i;
+	char	current_c;
+	int		carry;
+
+	length = ft_strlen(nbr_str);
+	carry = 1;
+	i = length;
+	while (--i >= 0)
+	{
+		current_c = nbr_str[i];
+		if (current_c >= '0' && current_c <= '9')
+		{
+			if (carry)
+			{
+				if (current_c == '9')
+					nbr_str[i] = '0';
+				else
+				{
+					nbr_str[i] = current_c + 1;
+					carry = 0;
+				}
+			}
+		}
+	}
+}
 
 static void	add_venv(t_venv **head, char *name, char *value)
 {
@@ -23,6 +52,8 @@ static void	add_venv(t_venv **head, char *name, char *value)
 		error_exit("Allocation mémoire échouée");
 	new_env->name = ft_strdup(name);
 	new_env->value = ft_strdup(value);
+	if (ft_strcmp(new_env->name, "SHLVL"))
+		increment_ascii(new_env->value);
 	new_env->next = *head;
 	*head = new_env;
 }
